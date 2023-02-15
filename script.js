950cd2825aafc8691748cce210a968c17f637630
EmelieHa <108530359+EmelieHa@users.noreply.github.com>
2023-02-15 07:35:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let ball3 = document.getElementById("b3");
 let ball4 = document.getElementById("b4");
 let ball5 = document.getElementById("b5");
 let grattis = document.getElementById("congrats");
+let summa = document.getElementById("summa");
 let num1;
 let num2;
 let num3;
@@ -15,11 +16,11 @@ let ballArr = [];
 let user = prompt('Vad heter du?');
 
 const playFunc = () => {
-    num1 = Math.floor(Math.random() * 5) +1;
-    num2 = Math.floor(Math.random() * 5) +1;
-    num3 = Math.floor(Math.random() * 5) +1;
-    num4 = Math.floor(Math.random() * 5) +1;
-    num5 = Math.floor(Math.random() * 5) +1;
+    num1 = Math.floor(Math.random() * 10) +1;
+    num2 = Math.floor(Math.random() * 10) +1;
+    num3 = Math.floor(Math.random() * 10) +1;
+    num4 = Math.floor(Math.random() * 10) +1;
+    num5 = Math.floor(Math.random() * 10) +1;
     ball1.textContent = num1;
     ball2.textContent = num2;
     ball3.textContent = num3;
@@ -27,20 +28,23 @@ const playFunc = () => {
     ball5.textContent = num5;
     ballArr.push(num1, num2, num3, num4, num5);
   grattis.textContent = fillFunc(ballArr);
-   ballArr = [];
+ 
     }
 
 playBtn.addEventListener('click', playFunc);
 
 
 const fillFunc = (arr) => {
-
-if (arr[0] === arr[1] || arr[0] === arr[2] || arr[0] === arr[3] || arr[0] === arr[4]) {
-    return 'Du är en vinnare, ' + user;
-
-} else {
-    return 'Otur, ' + user + '! Försök igen';
-}
+let resultArr = arr.reduce((accum, current) => accum+current, 0);
+let sumArr =+ resultArr;
+summa.textContent = 'Resultat = ' + sumArr;
+while (sumArr < 100) {
+    return 'Kör igen ' + user; 
 } 
+    'Du är en vinnare ' + user;
+
+}
+
+
 
 
